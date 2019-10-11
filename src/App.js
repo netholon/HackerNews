908c07b8fa32db1a6a42509bc5968a3fc6294d7d
netholon <44534268+netholon@users.noreply.github.com>
2019-10-11 15:29:53 -0700

    added routing to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import About from './components/About'
 
 import './App.css';
 
-function App() {
+const App = () => {
     return (
-        <div className="App">
-            <Navbar />
-        </div>
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Switch>
+                    <Route exact path='/'>
+                        <h1>Portfolio</h1>
+                    </Route>
+                    <Route exact path='/about'>
+                        <About /> 
+                    </Route>
+                    <Route exact path='/contact'>
+                        <h1>Contact Me</h1>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
