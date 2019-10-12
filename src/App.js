5b158f1d68f032fbca2b9ab785863382f600007c
netholon <44534268+netholon@users.noreply.github.com>
2019-10-11 19:19:05 -0700

    added portfolio data file for dynamic info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import About from './components/About'
+import portfolio_data from './portfolio_data'
 
 import './App.css';
 
@@ -15,7 +16,7 @@ const App = () => {
                         <h1>Portfolio</h1>
                     </Route>
                     <Route exact path='/about'>
-                        <About /> 
+                        <About data={portfolio_data} /> 
                     </Route>
                     <Route exact path='/contact'>
                         <h1>Contact Me</h1>
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,36 @@
 import React from 'react'
 
-const About = () => {
+const About = ({ data }) => {
+    const { name, email, phone} = data
+
     return (
         <div>
             <h1>About Me</h1>
+            
+            <ul className='about'>
+                <li>
+                    {name &&
+                        <div>
+                            <strong>Name: </strong> {name}  
+                        </div>
+                    }
+                </li>
+                <li>
+                    {email &&
+                        <div>
+                            <strong>Email: </strong> {email}  
+                        </div>
+                    }
+                </li>
+                <li>
+                    {phone &&
+                        <div>
+                            <strong>Phone: </strong> {phone}  
+                        </div>
+                    }
+                </li>
+            </ul>
+
         </div>
     )
 }
